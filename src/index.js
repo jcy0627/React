cb59e08de65ee2e144cea8d15a85afc8dbfafc48
jcy0627 <jcy0627@gmail.com>
2023-01-03 10:56:07 +0900

    comment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './react/Library';
+// import Library from './react/Library';
+// import Clock from './tick/Clock';
+
+import CommentList from './comment/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+setInterval(() => {
+
+  root.render(
+    <React.StrictMode>
+      <CommentList />
+    </React.StrictMode>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
